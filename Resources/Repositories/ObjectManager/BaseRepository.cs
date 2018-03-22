c0bbae38797a3b4b31a302f2238b54dae3264326
Diego Bernal <diego.bernal.182@gmail.com>
2018-03-22 12:59:35 -0500

    Creating repo structures

---
+++ Resources/Repositories/ObjectManager/BaseRepository.cs
@@ -0,0 +1,42 @@
+﻿using Relativity.API;
+using Relativity.Services.ArtifactGuid;
+using Resources.Repositories.Logging;
+
+namespace Resources.Repositories.ObjectManager
+{
+    public class BaseRepository
+    {
+        public IHelper _helper;
+        public int _workspaceID;
+
+        public ILoggingRepository LogRepo
+        {
+            get
+            {
+                if (_LogRepo == null)
+                    _LogRepo = new LoggingRepository(this._helper, this._workspaceID);
+                return _LogRepo;
+            }
+        }
+        private ILoggingRepository _LogRepo;
+
+        public int WorkspaceID
+        {
+            get
+            {
+                return this._workspaceID;
+            }
+        }
+
+        public IArtifactGuidManager ArtifactGuidClient
+        {
+            get
+            {
+                if (_ArtifactGuidClient == null)
+                    _ArtifactGuidClient = _helper.GetServicesManager().CreateProxy<IArtifactGuidManager>(ExecutionIdentity.CurrentUser);
+                return _ArtifactGuidClient;
+            }
+        }
+        private IArtifactGuidManager _ArtifactGuidClient;
+    }
+}
