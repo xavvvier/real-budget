e057f2cc895418246ab975c5ce7dd2590454a77a
Diego Bernal <diego.bernal.182@gmail.com>
2018-03-22 14:27:42 -0500

    adding cs

---
+++ Resources/EventHandlers/Document/DocumentPreSave.cs
@@ -0,0 +1,36 @@
+﻿using kCura.EventHandler;
+using Resources.Repositories;
+using Resources.Repositories.ObjectManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resources.Event_Handlers.Document
+{
+    class DocumentPreSave : PreSaveEventHandler
+    {
+        public override FieldCollection RequiredFields
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public override Response Execute()
+        {
+            throw new NotImplementedException();
+        }
+        protected IDataRepository _Repo
+        {
+            get
+            {
+                if (_repo == null)
+                    _repo = new ObjectManagerRepository(this.Helper, this.Helper.GetActiveCaseID());
+                return _repo;
+            }
+        }
+        private IDataRepository _repo;
+    }
+}
